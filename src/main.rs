341af60a28ee77cbabbc8c3388041ae17fa0f0bb
Stirling Hostetter <stirhoss@icloud.com>
2022-05-26 10:00:04 -0500

    Add clarification that the number is between 1 and 100

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the number!(between 1-100)");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
